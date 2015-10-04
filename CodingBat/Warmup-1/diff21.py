a43d435c9f16bd771cda20578f1c5ede377213b1
Tejas Vyas <tejasvya@buffalo.edu>
2015-10-03 22:56:38 -0400

    CodingBat: Warmup Exercises #1
    Diff-21, Near-Hundred, Missing-Char

---
+++ CodingBat/Warmup-1/diff21.py
@@ -0,0 +1,9 @@
+'''Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21. 
+
+diff21(19) ? 2
+diff21(10) ? 11
+diff21(21) ? 0'''
+
+def diff21(n):
+  if n<=21: return 21-n
+  else: return 2*(n-21)
