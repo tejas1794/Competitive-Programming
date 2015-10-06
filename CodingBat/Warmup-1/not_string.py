c42e0b08d2fe4872437ba2cda0dcbf71938ad17b
Tejas Vyas <tejasvya@buffalo.edu>
2015-10-05 21:38:48 -0400

    CodingBat: Warmup 1
    Makes-10, Not-String, Sum-Double

---
+++ CodingBat/Warmup-1/not_string.py
@@ -0,0 +1,11 @@
+'''
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. 
+
+not_string('candy') ? 'not candy'
+not_string('x') ? 'not x'
+not_string('not bad') ? 'not bad'
+'''
+
+def not_string(str):
+  if len(str)<3 or str[0] != 'n' and str[1] != 'o' and str[2] != 't': return "not " + str
+  else: return str
