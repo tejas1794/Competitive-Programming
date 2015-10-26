b1e02d0fa1dafe9988be738cb32de9e972fcfb59
Tejas Vyas <tejasvya@buffalo.edu>
2015-10-25 23:07:19 -0400

    Logic-2
    Added Make-Bricks

---
+++ CodingBat/Logic-2/make_bricks.py
@@ -0,0 +1,12 @@
+'''
+We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops.
+
+make_bricks(3, 1, 8) → True
+make_bricks(3, 1, 9) → False
+make_bricks(3, 2, 10) → True
+'''
+
+def make_bricks(small, big, goal):
+  return not((goal > (big*5) + small) or goal%5>small)
+
+
