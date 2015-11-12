b9d01cbd9c982e9d46c6ea5ee97fd530120f2671
Tejas Vyas <tejasvya@buffalo.edu>
2015-11-11 20:34:26 -0500

    LeetCode: sqrt(x)
    Added approach

--- LeetCode/sqrt(x).py
+++ LeetCode/sqrt(x).py
@@ -4,7 +4,7 @@ Implement int sqrt(int x).
 Compute and return the square root of x.
 '''
 
-
+#Approach: Simply using the inbuilt functionality of Python. Not the fastest idea, but should be the simplest.
 
 class Solution(object):
     def mySqrt(self, x):
