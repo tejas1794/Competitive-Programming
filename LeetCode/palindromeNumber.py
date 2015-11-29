45cfe53205ef5f06332fd68417db7b6e4e4ae733
Tejas Vyas <tejasvya@buffalo.edu>
2015-11-28 23:36:21 -0500

    LeetCode: Added palindromeNumber
    Added solution to the problem Palindrome Number

---
+++ LeetCode/palindromeNumber.py
@@ -0,0 +1,22 @@
+'''
+Determine whether an integer is a palindrome. Do this without extra space.
+
+MORE INFO BELOW (SPOILER ALERT!)
+
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+'''
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        return x[::-1] == x
