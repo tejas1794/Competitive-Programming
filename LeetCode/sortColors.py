d04095e96ae36aa94a870506176db2a0b7a861bc
Tejas Vyas <tejasvya@buffalo.edu>
2015-12-11 23:58:56 -0500

    LeetCode: Added sortColors
    Added solution to the problem Sort Colors.

---
+++ LeetCode/sortColors.py
@@ -0,0 +1,13 @@
+'''
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+'''
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
