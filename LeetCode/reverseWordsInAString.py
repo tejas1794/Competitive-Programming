8f2f5be176f596e0d9a5445da0d82c473201b8fe
Tejas Vyas <tejasvya@buffalo.edu>
2015-12-12 23:28:43 -0500

    LeetCode: Added reverseWordsInaString
    Added solution to the problem Reverse Words in a String.

---
+++ LeetCode/reverseWordsInAString.py
@@ -0,0 +1,16 @@
+'''
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+'''
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ' '.join(s.split()[::-1])
+        
