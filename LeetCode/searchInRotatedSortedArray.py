c15acf7a9dc9b4c02d258bb73c6a510df2007198
Tejas Vyas <tejasvya@buffalo.edu>
2015-12-14 23:36:00 -0500

    LeetCode: Added searchInRotatedSortedArray
    Added solution to the problem Search In a Rotated Sorted Array

---
+++ LeetCode/searchInRotatedSortedArray.py
@@ -0,0 +1,18 @@
+'''
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+'''
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target not in nums: return -1
+        else: return nums.index(target)
