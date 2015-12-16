2399a034b604a33c88082f0cfa579dd8faa836ff
Tejas Vyas <tejasvya@buffalo.edu>
2015-12-15 23:28:19 -0500

    LeetCode: added searchInRotatedSortedArrayII
    Added solution to the problem Search in a Rotated Sorted Array II

---
+++ LeetCode/searchInRotatedSortedArrayII.py
@@ -0,0 +1,16 @@
+'''
+Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array.
+'''
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        return target in nums
