fe5ea1da69d51afea26b5f746f2d2443da50fc01
Tejas Vyas <tejasvya@buffalo.edu>
2015-12-17 23:44:38 -0500

    Added Python domain
    Added directory and solution of the Hello World Problem

---
+++ HackerRank/Python/HelloWorld.py
@@ -0,0 +1,19 @@
+'''
+Problem Statement
+
+Python is an interactive language that allows us to execute statements individually and see their results. In other words, you don't have to write a full code and compile it before you see the results as in other languages like C++ and Java.
+
+Here is a sample line of code that can be executed in Python:
+
+print "Hello World!" 
+You can even store the string in a variable and then print it:
+
+my_string = "Hello World!"
+print my_string
+The above code will print Hello World! on your screen. You can try it in the console below.
+
+P.S. Do try Solve Me First and Solve Me Second in Python before moving ahead.
+'''
+
+#write your code in next line. 
+print "Hello World!" 
