ea7e7e63a65979935a34ae6e5f25c4cab78e40f7
Tejas Vyas <tejasvya@buffalo.edu>
2016-01-13 23:46:18 -0500

    HackerRank: Added Contest Question (30 Days of Code)
    Added Day 1 solution

---
+++ HackerRank/Contests/30 Days of Code/Day1.py
@@ -0,0 +1,45 @@
+'''
+Day 1: Data Types!
+
+For this challenge, consider the following inputs (you don't need to read any input):
+
+5.35
+'a'
+false
+100
+"I am a code monkey"
+true
+17.3
+'c'
+"derp"
+
+For each line above, print out if it is an instance of a primitive or referenced type as well as which data type it is (using the typical examples above).
+
+Note: This challenge is focused only on Java data types, but you can submit the answer in other languages as well. Hint: In Java, String is a referenced type. Most challenges in upcoming days are language-agnostic.
+
+Good luck!
+
+Output Format
+
+Output nine lines. Print one line for each of the input given above in the following format:
+
+MainDataTypeOfInstance : SpecificDataTypeOfInstance
+For example, if the input is:
+
+123
+Your output should be:
+
+Primitive : int
+Make sure you spell everything correctly and capitalize your entries. Also, don't forget spaces and colon in between the main and specific data types!
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+print "Primitive : double"
+print "Primitive : char"
+print "Primitive : boolean"
+print "Primitive : int"
+print "Referenced : String"
+print "Primitive : boolean"
+print "Primitive : double"
+print "Primitive : char"
+print "Referenced : String"
