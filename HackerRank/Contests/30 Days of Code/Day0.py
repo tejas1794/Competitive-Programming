5d6fa2aaf3b6ca1742e8965e8df5ab8ebad438e3
Tejas Vyas <tejasvya@buffalo.edu>
2016-01-13 23:45:24 -0500

    HackerRank: Added Contest - 30 Days of Code
    Added Day 0 solution

---
+++ HackerRank/Contests/30 Days of Code/Day0.py
@@ -0,0 +1,11 @@
+'''
+Day 0: Print Hello World!
+
+For this challenge, all you have to do is print the following two lines:
+
+Hello World. 
+Welcome to 30 Days of Code.
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+print 'Hello World.'
+print 'Welcome to 30 Days of Code.'
