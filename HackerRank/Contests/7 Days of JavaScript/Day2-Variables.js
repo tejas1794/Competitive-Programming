cd4fc4ed3e4488d742204a029abff61939099740
Tejas Vyas <tejasvya@buffalo.edu>
2016-02-19 00:00:34 -0500

    HackerRank: Added Contest Question (7 Days of JavaScript)
    Added Day 2 - Variables

---
+++ HackerRank/Contests/7 Days of JavaScript/Day2-Variables.js
@@ -0,0 +1,9 @@
+/*
+
+Task
+
+In the editor below, declare a variable named newVariable and assign it the following string, "My First Variable".
+*/
+
+//Write your code below this line.
+var newVariable = "My First Variable";
