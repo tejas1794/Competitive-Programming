02d4fa76b6f1772aa9d52a8b7e7499b70bcc7777
Tejas Vyas <tejasvya@buffalo.edu>
2016-02-18 23:59:17 -0500

    HackerRank: Added Contest Question (7 Days of JavaScript)
    Added Day 2 - Switch Statements

---
+++ HackerRank/Contests/7 Days of JavaScript/Day2-SwitchStatements.js
@@ -0,0 +1,56 @@
+/*
+Task
+
+You are given a variable, numnum. Your task is to print: 
+
+- ONE, if numnum is equal to 11. 
+- TWO, if numnum is equal to 22. 
+- THREE, if numnum is equal to 33.
+- FOUR, if numnum is equal to 44.
+- FIVE, if numnum is equal to 55.
+- SIX, if numnum is equal to 66.
+- SEVEN, if numnum is equal to 77.
+- EIGHT, if numnum is equal to 88.
+- NINE, if numnum is equal to 99.
+- PLEASE TRY AGAIN, if numnum is none of the above.
+
+Note: Do not declare the variable numnum; it is declared inside our code checker. Use console.log to print statements to the console.
+
+*/
+
+//Do not declare variable num.
+//Write your code below this line.
+switch(num){
+    case 1:
+        console.log("ONE");
+        break;
+    case 1:
+        console.log("ONE");
+        break;
+    case 2:
+        console.log("TWO");
+        break;
+    case 3:
+        console.log("THREE");
+        break;
+    case 4:
+        console.log("FOUR");
+        break;
+    case 5:
+        console.log("FIVE");
+        break;
+    case 6:
+        console.log("SIX");
+        break;
+    case 7:
+        console.log("SEVEN");
+        break;
+    case 8:
+        console.log("EIGHT");
+        break;
+    case 9:
+        console.log("NINE");
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN");
+}
