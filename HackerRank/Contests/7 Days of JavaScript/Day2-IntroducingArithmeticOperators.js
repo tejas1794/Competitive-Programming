11e04de4481737ecd096acaa34c8b5c7af414674
Tejas Vyas <tejasvya@buffalo.edu>
2016-02-19 00:02:18 -0500

    HackerRank: Added Contest Question (7 Days of JavaScript)
    Added Day 2 - Introducing Arithmetic Operators

---
+++ HackerRank/Contests/7 Days of JavaScript/Day2-IntroducingArithmeticOperators.js
@@ -0,0 +1,26 @@
+/*
+Task
+
+You are given two number variables a and b. Both of them have been declared and assigned.
+
+Your task is to :
+
+Create a variable add and assign it with the summation of a and b.
+Create a variable sub and assign it with the subtraction of b from a.
+Create a variable mul and assign it with the multiplication of a and b.
+Create a variable div and assign it with the division of a by b.
+Create a variable inc and assign it with the incremented value of a.
+Create a variable dec and assign it with the decremented value of b.
+Note: Do not create variables a and b. They have already been created and assigned inside our code checker.
+*/
+
+//Do not declare variables `a` and `b`.
+//Write your code below this line.
+var add = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+var inc = a;
+inc++;
+var dec = b;
+dec--;
