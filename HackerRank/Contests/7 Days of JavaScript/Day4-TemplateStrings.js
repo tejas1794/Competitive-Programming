5c43adaa209a35bfb582ee64bf58a08a39b5ef29
Tejas Vyas <tejasvya@buffalo.edu>
2016-02-23 00:02:13 -0500

    HackerRank: Added Contest Question (7 Days of JavaScript)
    Added Day 4 - Template Strings

---
+++ HackerRank/Contests/7 Days of JavaScript/Day4-TemplateStrings.js
@@ -0,0 +1,19 @@
+/*Task
+
+Your task is to create a template string and assign it to the variable my_template_string.
+
+The template string should contain the following data only:
+
+My name is ${my_name}.
+My id is ${my_id}.
+My address is ${my_address}.
+Note
+
+Do not create any variable other than my_template_string.
+Do not print anything on the console.
+Replace the blank (_______________________) with the template string.
+*/
+var my_template_string = `My name is ${my_name}.
+My id is ${my_id}.
+My address is ${my_address}.`;
+
