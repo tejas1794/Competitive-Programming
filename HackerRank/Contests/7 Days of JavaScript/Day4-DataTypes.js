196725b3f033f82d8858d61af289b86b247add9a
Tejas Vyas <tejasvya@buffalo.edu>
2016-02-22 23:59:14 -0500

    HackerRank: Added Contest Question (7 Days of JavaScript)
    Added Day 4 - Data Types

---
+++ HackerRank/Contests/7 Days of JavaScript/Day4-DataTypes.js
@@ -0,0 +1,12 @@
+/*Task
+
+Your task is to:
+
+Create a variable my_num and assign it any number.
+Create a variable my_bool and assign it any boolean.
+Create a variable my_str and assign it any string.*/
+
+//Write your code below this line.
+var my_num = 12;
+var my_bool = true;
+var my_str = "string";
