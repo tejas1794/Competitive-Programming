1f5b91787e09e725d7a054c7870ed1688fa3ce37
Tejas Vyas <tejasvya@buffalo.edu>
2016-02-24 22:37:23 -0500

    HackerRank: Added Contest Question (7 Days of JavaScript)
    Added Day 5 - Const

---
+++ HackerRank/Contests/7 Days of JavaScript/Day5-Const.js
@@ -0,0 +1,12 @@
+/*Task
+
+Your task is to declare the following constants:
+
+PI and initialize it with Math.PI .
+EULER and initialize it with Math.E .
+SQRT2 and initialize it with Math.SQRT2 .
+*/
+//Write your code below this line.
+const PI = Math.PI;
+const EULER = Math.E;
+const SQRT2 = Math.SQRT2;
